ca71507ccbbe887702b355c32d5d5c6d606f5d99
bryanstark1 <bryandstark@icloud.com>
2023-08-14 14:15:17 -0400

    Fixed href error in Navbar

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,21 +4,23 @@ export default function Hero() {
   return (
     <section id="hero">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
-        <div className="lg:flex-grow md:w-1/2 lg:pr-24 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-white">Hi, I'm Bryan.</h1>
-          <h1 className="title-font sm:text-2xl font-medium text-3xl text-white">Fullstack Software Engineer</h1>
-          <h1 className="title-font sm:text-2xl font-medium text-3xl mb-4 text-white">Front-End Web Developer</h1>
-          <div className="flex justify-center">
-            <a
-              href="#contact"
-              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg whitespace-nowrap">
-              Contact Me
-            </a>
-            <a
-              href="#projects"
-              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg whitespace-nowrap">
-              See My Past Work
-            </a>
+        <div className="lg:flex-grow md:w-1/2 lg:pr-24 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center flex-grow">
+          <div className='info-container p-5'>
+            <h1 className="title-font sm:text-4xl text-3xl mb-8 font-medium text-white">Hi, I'm Bryan.</h1>
+            <h1 className="title-font sm:text-2xl font-medium text-3xl text-white">Fullstack Software Engineer</h1>
+            <h1 className="title-font sm:text-2xl font-medium text-3xl mb-4 text-white">Front-End Web Developer</h1>
+            <div className="flex justify-center">
+              <a
+                href="#contact"
+                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg whitespace-nowrap">
+                Contact Me
+              </a>
+              <a
+                href="#projects"
+                className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg whitespace-nowrap">
+                See My Past Work
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/data.js
+++ src/data.js
@@ -1,11 +1,10 @@
-// 900 x 600 dimensions for project images
 export const projects = [
   {
     title: "NoiseNebula",
     subtitle: "MERN + TypeScript",
     description:
       "A SoundCloud-style, music sharing application where users can upload, organize, edit, and listen to their music while sharing it with other users across the app.",
-    image: "./NoiseNebulaPhoto.jpg",
+    image: "./NoiseNebula.png",
     link: "https://pythonbootcamp.com",
     github: "https://github.com/bryanstark1/noisenebula",
   },
@@ -14,7 +13,7 @@ export const projects = [
     subtitle: "MERN Stack",
     description:
       "A single-page, full stack, MERN application for small businesses to consolidate and track items across their inventory, with token-based user authentication.",
-    image: "./InventoryTrackerPhoto.jpg",
+    image: "./InventoryTracker.png",
     link: "https://sei-inventory-manager-c8b2fde1ac37.herokuapp.com/",
     github: "https://github.com/drios2023-ga/inventory-management",
   },
@@ -23,28 +22,28 @@ export const projects = [
     subtitle: "JavaScript + Express + Mongoose",
     description:
       "A digitized list of sneakers with full CRUD capabilities for compiling data on your collection. Responsive to varied screen sizes.",
-    image: "./SneakerClosetPhoto.jpg",
+    image: "./SneakerCloset.png",
     link: "https://sneakercloset-bdd404a62af9.herokuapp.com/",
     github: "https://github.com/bryanstark1/sneaker-closet",
   },
-  {
-    title: "myRestaurants",
-    subtitle: "Python + Django + PostgreSQL",
-    description:
-      "A tool for tracking your favorite restaurants. A one-to-many relationship between data models allow for authenticated users to build their own unique lists.",
-    image: "./project-2.gif",
-    link: "https://myrestaurants-db1af5d71fe8.herokuapp.com/",
-    github: "https://github.com/bryanstark1/myrestaurants",
-  },
   // {
-  //   title: "Space Battle",
-  //   subtitle: "JavaScript + HTML/CSS",
+  //   title: "myRestaurants",
+  //   subtitle: "Python + Django + PostgreSQL",
   //   description:
-  //     "A browser game built solely on JavaScript. Battle against a randomly generated number of enemy ships and try to beat your high score!",
-  //   image: "./SpaceBattlePhoto.jpg",
-  //   link: "https://pythonbootcamp.com",
-  //   github: "https://github.com/bryanstark1/space-battle",
+  //     "A tool for tracking your favorite restaurants. A one-to-many relationship between data models allow for authenticated users to build their own unique lists.",
+  //   image: "./project-2.gif",
+  //   link: "https://myrestaurants-db1af5d71fe8.herokuapp.com/",
+  //   github: "https://github.com/bryanstark1/myrestaurants",
   // },
+  {
+    title: "Space Battle",
+    subtitle: "JavaScript + HTML/CSS",
+    description:
+      "A browser game built solely on JavaScript. Battle against a randomly generated number of enemy ships and try to beat your high score!",
+    image: "./SpaceBattle.png",
+    link: "https://space-battle-1.netlify.app/",
+    github: "https://github.com/bryanstark1/space-battle",
+  },
 ];
 
 export const skills = [
